a281e094b199502026d3062b69b4d915717e79dc
Felipe Teixeira <felipesantosteixera@hotmail.com>
2017-08-10 22:41:30 -0300

    a

--- js/controllers/loginCtrl.js
+++ js/controllers/loginCtrl.js
@@ -9,7 +9,16 @@
     function LoginController($scope) {
 
         var init = function() {
+            $scope.inputType = 'password';
 
+            $scope.hideShowPassword = function() {
+                var type = $scope.inputType  == 'password' ? "text" : "password";
+                $scope.inputType = type;
+
+                setTimeout(function () {
+                    $("#login-password").focus();
+                }, 10);
+            };
         };
 
         init();
