7f9caedb255997d635e050300d53a7263b96485f
Felipe Teixeira <felipesantosteixera@hotmail.com>
2017-09-03 22:13:19 -0300

    aa

--- js/controllers/masterCtrl.js
+++ js/controllers/masterCtrl.js
@@ -4,9 +4,9 @@
     angular.module('offy-app')
       .controller('MasterController', MasterController);
 
-    MasterController.$inject = ['$scope', '$location'];
+    MasterController.$inject = ['$scope', '$location', '$rootScope'];
 
-    function MasterController($scope, $location) {
+    function MasterController($scope, $location, $rootScope) {
         $scope.$location = $location;
 
         var init = function() {
@@ -14,6 +14,18 @@
         };
 
         init();
+
+    //     $rootScope.$on('$stateChangeSuccess',
+    //     function(event, toState, toParams, fromState, fromParams) {
+    //       $state.current = toState;
+    //     }
+    //   )
+
+    $rootScope.$on('$stateChangeSuccess', function(event, to, toParams, from, fromParams) {
+        $rootScope.$previousState = from;
+    });
+
     }
 
 })();
+
